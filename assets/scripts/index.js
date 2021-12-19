f99af3cd42d7ba0aa4ddf59f620b0e1ebf875bf4
Leonardo <demetrio.vd@hotmail.com>
2021-12-19 19:34:26 -0300

    Main content

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,6 @@
 const light = document.querySelector(".light");
 const grid = document.querySelector("#hex-grid");
-
+const backEffect = document.querySelector("[backEffect]");
 
 grid.addEventListener('mousemove', function(e){
     light.style.left = `${e.clientX}px`;
@@ -8,7 +8,10 @@ grid.addEventListener('mousemove', function(e){
 });
 
 
-
+backEffect.addEventListener('mousemove', (e) => {
+    light.style.left = `${e.clientX}px`;
+    light.style.top = `${e.clientY}px`;
+});
 
 
 
